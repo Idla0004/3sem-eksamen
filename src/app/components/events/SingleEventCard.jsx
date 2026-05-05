587342141ac1fd349dcca7eea93a2018feb0e15e
Ida <idla0004@stud.kea.dk>
2026-05-05 15:01:39 +0200

    SingleEventCard html opbygget, styling mangler

---
+++ src/app/components/events/SingleEventCard.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+const SingleEventCard = () => {
+  return (
+    <div>
+      <Image
+        src=""
+        alt=""
+        width={450}
+        height={250}
+      />
+      <h3></h3>
+      <h2>
+        <span>|</span>
+        <span></span>
+      </h2>
+      <p></p>
+      <button></button>
+    </div>
+  );
+};
+
+export default SingleEventCard;
